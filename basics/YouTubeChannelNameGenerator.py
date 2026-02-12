eefce777e978a8326ac6668b4aa1dba7a524892e
safa-bamousa <safacode2006@gmail.com>
2026-02-12 13:33:05 +0100

    Add a new file

---
+++ basics/YouTubeChannelNameGenerator.py
@@ -0,0 +1,5 @@
+#This is an exercice from OctuCode
+print("Welcome to the YouTube Channel Name Genarator:")
+name = input("What is your nickname? ") #Name of the user
+channel_about = input("What is your channel about? ") #Subjet of the user's channel
+print("You could name your channel ("+ channel_about + " with " + name + ")")
