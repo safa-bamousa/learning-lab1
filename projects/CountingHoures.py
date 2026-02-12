84595f1f41630433fc43b61ae235820c4bcd84eb
safa-bamousa <safacode2006@gmail.com>
2026-02-12 14:44:31 +0100

    Add a new file

---
+++ projects/CountingHoures.py
@@ -0,0 +1,4 @@
+user_minutes = int( input("Please type the number of minutes: "))
+houres = user_minutes // 60
+minutes = user_minutes % 60
+print("You have", houres , "houres and", minutes, "minutes.")
