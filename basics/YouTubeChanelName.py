aa23b7fab02cc88cf94b9bcbbefb67628ada999e
safa-bamousa <safacode2006@gmail.com>
2026-02-12 13:29:38 +0100

    Add some modifications

---
+++ basics/YouTubeChanelName.py
@@ -0,0 +1,5 @@
+#This is an exercice from ouctocode
+print("Welcome to the YouTube Channel Name Genarator:")
+name = input("What is your nickname? ") #THe name of the user
+channel_about = input("What is your channel about? ")
+print("You could name your channel ("+ channel_about + " with " + name + ")")
--- basics/variables.py
+++ basics/variables.py
@@ -3,5 +3,11 @@ y = 5
 print(x + y)
 print("I love Python \nPython is very easy \nThis is a new line")
 
-name=input("What is your name? \n")
-print("Hello " + name)
+text = "Hello"
+print(len(text))
+
+name = input("What is your name? \n")
+print("Hello " + name + ", nice to meet you!") 
+name_len = len(name)
+print("The len of your name is: ")
+print(name_len)
