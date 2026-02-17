2bd8e6bdf325cc9296aa4604919cb71963ff022c
safa-bamousa <safacode2006@gmail.com>
2026-02-17 23:40:12 +0000

    Add a new file

--- projects/EncryptMessage.py
+++ projects/EncryptMessage.py
@@ -9,22 +9,38 @@ def encrypt_message(message, key):
         if letter.islower():
             original_position = alphabet.index(letter)
             encrypted_word += alphabet[(original_position + key) % 26]
-            
         elif letter.isupper() :
             original_position = alphabet1.index(letter)
             encrypted_word += alphabet1[(original_position + key) % 26]
                 #(Index+Moves)%Total?
-        
         else :
             encrypted_word += letter
     print(encrypted_word)
 
-message = input("Please type a message: ")
+def decrypt_message(message, key):
+    encrypted_word = ""
+    for letter in message:
+        if letter.islower():
+            original_position = alphabet.index(letter)
+            encrypted_word += alphabet[(original_position - key) % 26]
+        elif letter.isupper() :
+            original_position = alphabet1.index(letter)
+            encrypted_word += alphabet1[(original_position - key) % 26]
+                #(Index+Moves)%Total?
+        else :
+            encrypted_word += letter
+    print(encrypted_word)
+
+
+message = input("Please type a message to encrypt: ")
 key = int(input("Enter a shift number please: "))
 
 encrypt_message(message, key)
 
+message = input("Please type a message to decrypt: ")
+key = int(input("Enter a shift number please: "))
 
+decrypt_message(message, key)
 
 
 
